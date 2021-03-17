75eb17bfe32897b191bc1e9bd05304d345af5af9
liamlawless <limlawless@gmail.com>
2021-03-16 22:38:32 -0500

    Added comments and stuff for u guys

--- public/javascripts/classes.js
+++ public/javascripts/classes.js
@@ -26,10 +26,16 @@ class DrawingObject {
 // }
 
 class Pen extends DrawingObject {
+    // Pen:
+    //  id: drawingObject id
+    //  lowerLeft: bounding rectangle lower left corner (for select)
+    //  upperRight: bounding rectangle upper right corner
+    //  size: pen stroke-width
     constructor(id, lowerLeft, upperRight, size, color) {
         super(id, DEF_POS, DEF_ROTATE, DEF_SCALE, lowerLeft, upperRight);
         this.size = size;
         this.color = color;
+        // Set up the SVG path
         this.path = document.createElementNS("http://www.w3.org/2000/svg", "path");
         this.path.setAttribute("stroke-width", this.size);
         this.path.setAttribute("fill", "none");
@@ -41,25 +47,39 @@ class Pen extends DrawingObject {
     getSvg() {
         return this.path;
     }
+
+    // Set the path string
     setPath(path){
         this.path.setAttribute("d", path);
     }
+
+    // Get the path string
     getPath(){
         return this.path.getAttribute("d");
     }
+
+    // Add points to the path
     updatePathData(newpoints) {
         let pathString;
+        if(newpoints.length == 0){
+            return;
+        }       
+        // if path just started, add the moveTo
         if (this.path.getAttribute("d") == "") {
             pathString = "M " + newpoints[0].x + " " + newpoints[0].y;
         }
+        // if path already started, add some lineTo's
         else {
+            // lineTo
             if (newpoints[0].type == "line") {
                 pathString = "L " + newpoints[0].x + " " + newpoints[0].y;
             }
+            // jump/moveTo (for the mouse leaving board and coming back case)
             else if (newpoints[0].type == "jump") {
                 pathString = "M " + newpoints[0].x + " " + newpoints[0].y;
             }
         }
+        // add the rest of the new points if there are any
         for (let i = 1; i < newpoints.length; i++) {
             if (newpoints[i].type == "line") {
                 pathString += " L " + newpoints[i].x + " " + newpoints[i].y;
@@ -68,6 +88,7 @@ class Pen extends DrawingObject {
                 pathString += " M " + newpoints[i].x + " " + newpoints[i].y;
             }
         }
+        // update the path string for this pen
         this.path.setAttribute("d", this.path.getAttribute("d") + pathString);
     }
 }
